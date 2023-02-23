14eabb559c12a54d94016b2a8e7ef084fb0f5b5a
Maruf <maruf.shellbeehaken@gmail.com>
2023-02-23 17:59:18 +0600

    at last react

---
+++ react/my-app/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello';
+import Demo from './components/Demo';
+
+function App() {
+  if (false) {
+    return <Hello />
+  } else
+  return <Demo/>
+}
+
+export default App;
---
+++ react/my-app/src/components/Demo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './hello.css'
+
+export default function Demo() {
+  return (
+    <div className='hudai'>
+        <h1>This is a demo div</h1>
+    </div>
+  )
+}
